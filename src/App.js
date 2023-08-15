d827cb6afcfe09380b58aeccfb5523659d025b8a
Danu710 <danukr710@gmail.com>
2023-08-15 12:14:42 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+// import TodoForm from './component/TodoForm';
+import TodoList from './component/TodoList';
+
+function App() {
+  return (
+    <div className="todo-app">
+      {/* <TodoForm/> */}
+      <TodoList/>
+    </div>
+  );
+}
+
+export default App;
